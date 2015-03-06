060c33e3646e79c7dfb015ce8b86ce95b9d375bd
Valder Gallo <valdergallo@gmail.com>
2015-03-05 22:43:36 -0300

    Update urls

--- urls.py
+++ urls.py
@@ -1,6 +1,11 @@
 # coding: utf-8
 from main.views import MainHandler
+import tornado.web
+import settings
 
 routes = [
     (r"/", MainHandler),
+    # static files
+    (r'/static/(.*)', tornado.web.StaticFileHandler,
+        {'path': settings.CONFIG.get('static_path')}),
 ]
