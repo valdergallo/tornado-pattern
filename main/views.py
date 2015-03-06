1bf4b53c1fe2e0ef9ad0ec36a4331903a20eca77
Valder Gallo <valdergallo@gmail.com>
2015-03-05 22:53:49 -0300

    Update async

--- main/views.py
+++ main/views.py
@@ -1,7 +1,21 @@
 import tornado.web
+import tornado.httpclient
 
 
 class MainHandler(tornado.web.RequestHandler):
+
     def get(self):
         items = ["Item 1", "Item 2", "Item 3"]
         self.render("index.html", title="My title", items=items)
+
+
+class AsyncHandler(tornado.web.RequestHandler):
+
+    @tornado.web.asynchronous
+    def get(self):
+        client = tornado.httpclient.AsyncHTTPClient()
+        client.fetch('http://groovematic.com/', callback=self._on_finish)
+
+    def _on_finish(self, response):
+        self.write(str(response.code))
+        self.finish()
--- urls.py
+++ urls.py
@@ -1,10 +1,12 @@
 # coding: utf-8
 from main.views import MainHandler
+from main.views import AsyncHandler
 import tornado.web
 import settings
 
 routes = [
     (r"/", MainHandler),
+    (r"/async/", AsyncHandler),
     # static files
     (r'/static/(.*)', tornado.web.StaticFileHandler,
         {'path': settings.CONFIG.get('static_path')}),
