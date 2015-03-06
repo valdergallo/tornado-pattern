61e53a65ce5d6fededd86b52960a1051a2c6fb87
Valder Gallo <valdergallo@gmail.com>
2015-03-05 23:11:18 -0300

    Add command --port

--- run.py
+++ run.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
 # coding: utf-8
 import tornado.ioloop
+from tornado.options import define
+from tornado.options import options
 from urls import routes
 import settings
+
+define("port", default=8000, help="run on the given port", type=int)
 
 application = tornado.web.Application(routes, **settings.CONFIG)
 
 if __name__ == "__main__":
-    application.listen(8888)
+    tornado.options.parse_command_line()
+    application.listen(options.port)
     tornado.ioloop.IOLoop.instance().start()
